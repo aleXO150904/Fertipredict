28e76fb54e68d067d9ba6602afbc1779eb116ed5
aleXO150904 <alexretamal7890@gmail.com>
2026-07-01 00:22:59 -0500

    Eliminando bloqueos CORS en Controllers

--- Backend FertiPredict/app/src/main/java/com/fertipredict/app/Dashboard/DashboardController.java
+++ Backend FertiPredict/app/src/main/java/com/fertipredict/app/Dashboard/DashboardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:8080"})
+
 @RestController
 @RequestMapping("/api/dashboard")
 @RequiredArgsConstructor
--- Backend FertiPredict/app/src/main/java/com/fertipredict/app/Prediction/PredictionController.java
+++ Backend FertiPredict/app/src/main/java/com/fertipredict/app/Prediction/PredictionController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:8080"})
+
 @RestController
 @RequestMapping(value = "/api/v1/prediction")
 @RequiredArgsConstructor
